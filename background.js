88d9ac988a7dcfb596d29bbbfaa898b84badee9c
Lee Packham <9469+leepa@users.noreply.github.com>
2020-03-02 09:56:57 +0000

    Semi-standard the files

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.status === 'complete') {
-        chrome.tabs.get(tabId, function(tab) {
-            var tablink = tab.url;
-            let re = /^https:\/\/([a-z0-9-]+)?(?:\.)?console\.aws\.amazon\.com\/([a-z0-9-]+)\/([a-z0-9]+(?=\/))?.*/;
-            let m = re.exec(tablink);
-            if (!m || m.length !== 4) return;
+  if (changeInfo.status === 'complete') {
+    chrome.tabs.get(tabId, function (tab) {
+      var tablink = tab.url;
+      const re = /^https:\/\/([a-z0-9-]+)?(?:\.)?console\.aws\.amazon\.com\/([a-z0-9-]+)\/([a-z0-9]+(?=\/))?.*/;
+      const m = re.exec(tablink);
+      if (!m || m.length !== 4) return;
 
-            let svc = m[2];
-            if (svc === "codesuite") svc = m[3];
+      let svc = m[2];
+      if (svc === 'codesuite') svc = m[3];
 
-            chrome.tabs.executeScript(tabId, {
-                file: 'change.js',
-            }, function() {
-                const u = chrome.runtime.getURL("services/" + svc + ".png");
-                fetch(u)
-                    .then(function(response) {
-                        if (response.ok) {
-                            chrome.tabs.sendMessage(tabId, {scriptOptions: {url: u}});
-                        }
-                    }).catch(function(error) {});
-            });
-        });
-    }
+      chrome.tabs.executeScript(tabId, {
+        file: 'change.js'
+      }, function () {
+        const u = chrome.runtime.getURL('services/' + svc + '.png');
+        fetch(u)
+          .then(function (response) {
+            if (response.ok) {
+              chrome.tabs.sendMessage(tabId, { scriptOptions: { url: u } });
+            }
+          }).catch(function (error) {});
+      });
+    });
+  }
 });
--- change.js
+++ change.js
@@ -1,19 +1,19 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    var scriptOptions = message.scriptOptions;
-    document.head = document.head || document.getElementsByTagName('head')[0];
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  var scriptOptions = message.scriptOptions;
+  document.head = document.head || document.getElementsByTagName('head')[0];
 
-    function changeFavicon(src) {
-        var x = document.querySelector("link[rel*='shortcut icon']");
-        if (x) document.head.removeChild(x);
+  function changeFavicon (src) {
+    var x = document.querySelector("link[rel*='shortcut icon']");
+    if (x) document.head.removeChild(x);
 
-        var link = document.createElement('link'),
-        oldLink = document.querySelector("link[rel*='icon']");
-        link.id = 'dynamic-favicon';
-        link.rel = 'icon';
-        link.href = src;
-        if (oldLink) document.head.removeChild(oldLink);
-        document.head.appendChild(link);
-    }
+    var link = document.createElement('link');
+    var oldLink = document.querySelector("link[rel*='icon']");
+    link.id = 'dynamic-favicon';
+    link.rel = 'icon';
+    link.href = src;
+    if (oldLink) document.head.removeChild(oldLink);
+    document.head.appendChild(link);
+  }
 
-    changeFavicon(scriptOptions.url);
+  changeFavicon(scriptOptions.url);
 });
